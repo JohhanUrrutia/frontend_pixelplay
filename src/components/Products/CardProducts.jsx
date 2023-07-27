2146e1dab3b012f3c29c683f2d996394501464d9
Johhan Urrutia <johhanurrutia@192.168.1.16>
2023-07-27 15:21:16 -0400

    fix url test netlify cardproducts

--- src/components/Products/CardProducts.jsx
+++ src/components/Products/CardProducts.jsx
@@ -16,7 +16,7 @@ const CardProducts = (props) => {
   return (
     <>
       <Link
-        to={`${import.meta.env.VITE_API_URL}/products/${props.to}`}
+        to={`${import.meta.env.NETLIFY_URL}/products/${props.to}`}
         style={{textDecoration: "none"}}
       >
         <Card
